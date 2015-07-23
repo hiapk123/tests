d26de3f2f7a77c3399f8dfa302fdb40ee7f39ea8
hiapk123 <hips2011@qq.com>
2015-07-23 21:44:16 +0800

    sqlhelper冲突

--- sms/src/org/uestc/util/SqlHelper.java
+++ sms/src/org/uestc/util/SqlHelper.java
@@ -6,13 +6,17 @@ import java.util.List;
 import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ArrayListHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+
+import com.uestc.bean.Users;
 
 public final class SqlHelper {
 
 	public static List<Object[]> find(String sql,Object ...params) {
 		List<Object[]> list=null;
+		QueryRunner runner=null;
 		try {
-			QueryRunner runner = new QueryRunner(JdbcUtils.getInstance().getDataSource());
+			runner= new QueryRunner(JdbcUtils.getInstance().getDataSource());
 		    list = runner.query(sql, new ArrayListHandler(),params);
 		} catch (SQLException e) {
 			
@@ -43,55 +47,6 @@ public final class SqlHelper {
 	
 	public static void main(String[] args) {
 		//test
-		for (int j = 0; j < 5000; j++) {
-			List<Users> list=findBean(Users.class, "select * from users");
-			System.out.println(j);
-			for(int i=0;i<list.size();i++){
-				System.out.println(list.get(i).getU_age());
-			}
-		}
 		
-		
-		return null;
-	}
-
-		
-	// ͳһ��select��䣬Ϊ���ܹ����ʽ�����������������ArrayList����������ֱ�ӹر���Դ
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	
-	public ArrayList executeQuery(String sql, String[] parameters) {
-		ArrayList results = new ArrayList();
-
-		try {
-			conn = jdbcUtils.getConnection();
-			ps = conn.prepareStatement(sql);
-
-			if (parameters != null) {
-				for (int i = 0; i < parameters.length; i++) {
-					ps.setString(i + 1, parameters[i]);
-				}
-			}
-
-			rs = ps.executeQuery();
-
-			ResultSetMetaData rsmd = rs.getMetaData();
-			int column = rsmd.getColumnCount();
-
-			while (rs.next()) {
-				Object[] objects = new Object[column];
-
-				for (int i = 1; i <= column; i++) {
-					objects[i - 1] = rs.getObject(i);
-				}
-
-				results.add(objects);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e.getMessage());
-		} finally {
-			jdbcUtils.free(conn, ps, rs);
-		}
-		return results;
 	}
 }
