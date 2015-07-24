5ba904ecb55dd59bb09f923c4b23bf37ec649bf4
mbt <mbt@url-forwarding.moniker.com>
2015-07-24 17:25:57 +0800

    添加引入

--- sms/src/org/uestc/util/SqlHelper.java
+++ sms/src/org/uestc/util/SqlHelper.java
@@ -1,6 +1,11 @@
 package org.uestc.util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.dbutils.DbUtils;
