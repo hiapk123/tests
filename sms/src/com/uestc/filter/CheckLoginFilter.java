ef4a279c5a80a3fa7991a513eff859d643a3f1b6
hiapk123 <hips2011@qq.com>
2015-10-12 16:14:24 +0800

    2015-10-12,zxt修改， -------- |过滤器修改	| --------

--- sms/src/com/uestc/filter/CheckLoginFilter.java
+++ sms/src/com/uestc/filter/CheckLoginFilter.java
@@ -15,7 +15,7 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-@WebFilter(filterName="checkLoginFilter",urlPatterns="/*")
+
 public class CheckLoginFilter implements Filter {
 	protected FilterConfig filterConfig = null;
 	private String redirectURL = null;
